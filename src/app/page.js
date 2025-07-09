562a94f56a5dafeb4ea014e9c8ab29a780b1ef40
aerissat <aerissat@gmail.com>
2025-07-09 11:02:29 +0300

    Updates page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
-import TradingGame from '../components/TradingGame';
+// src/app/page.js
+import TradingGame from './components/TradingGame';
 
 export default function Page() {
   return <TradingGame />;
